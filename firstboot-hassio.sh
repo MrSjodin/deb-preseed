1f9237674ef30f6476de1af55b72e1672d417174
MrSjodin <48206613+MrSjodin@users.noreply.github.com>
2022-02-15 14:15:53 +0100

    Update firstboot-hassio.sh

--- firstboot-hassio.sh
+++ firstboot-hassio.sh
@@ -2,7 +2,7 @@
 #
 # Script to run at first boot after installation
 # Install Hass.IO
-curl -sL "https://raw.githubusercontent.com/home-assistant/hassio-installer/master/hassio_install.sh" | bash -s
+curl -sL "https://raw.githubusercontent.com/MrSjodin/deb-preseed/main/install-hass-supervised.sh" | bash -s
 # Below to remove the script from crontab file and the script itself
 /bin/cat /etc/crontab | /bin/grep -v firstboot > /etc/crontab.tmp
 /bin/rm -f /etc/crontab
